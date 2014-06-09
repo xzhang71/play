bfa4c6916b929533a6bfa23b868f494dd5d16dea
xzhang <xzhang@bostontechnologies.com>
2014-06-09 16:59:15 -0400

    2014/6/9

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Created by xzhang71 on 6/3/14.
  */
 public class Main {
     public static void main(String[] args) {
-
+        StringBuffer sb = new StringBuffer();
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return 0;
+            }
+        };
+        Queue<Integer> pq = new PriorityQueue<Integer>(10, comp);
     }
 }
