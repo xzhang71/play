8fa7da20e5fb8cf2b03f5da0017570a84f361d6e
xzhang <xzhang@bostontechnologies.com>
2014-06-12 15:05:38 -0400

    2014/6/12

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,17 @@
-import java.util.*;
-
 /**
  * Created by xzhang71 on 6/3/14.
  */
 public class Main {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
-        Comparator<Integer> comp = new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return 0;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= 9999; i += 8) {
+            for (int j = 1; j <= 9999; j += 8) {
+                int t = (i + j) % 6;
+                if (t == 1) {
+                    System.out.println(i + ", " + j + ", " + t);
+                }
             }
-        };
-        Queue<Integer> pq = new PriorityQueue<Integer>(10, comp);
-        Stack<Integer> stack = new Stack<Integer>();
-        List<Integer> list = new ArrayList<>();
-        int[] nums = {0, 1, 2, 3, 4, 5, 6, 7};
+        }
     }
 }
