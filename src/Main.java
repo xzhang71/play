cf999b71dc2c7984852da79ebbe9456b29743e4c
xzhang <xzhang@bostontechnologies.com>
2014-06-12 15:01:57 -0400

    2014/6/12

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by xzhang71 on 6/3/14.
  */
 public class Main {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         Comparator<Integer> comp = new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 return 0;
             }
         };
-        Queue<Integer> pq = new PriorityQueue<Integer>(10, comp);
-        Stack<Integer> stack = new Stack<Integer>();
+        Queue<Integer> pq = new PriorityQueue<>(10, comp);
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> set = new HashSet<>();
     }
 }
