14bd783b4d14e3b6b390e53d9216dae981df5f32
xzhang <xzhang@bostontechnologies.com>
2014-06-13 16:52:20 -0400

    2014/6/13

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by xzhang71 on 6/3/14.
  */
 public class Main {
     public static void main(String[] args) {
-        List<Integer> l = new ArrayList<>();
-        System.out.println(l.lastIndexOf(1));
+        List<String> list = new ArrayList<>();
+
+        Map<String, String> map = new HashMap<>();
+
+        for(int i = 0, j = 0; i + j < 10; i++, j++) {
+            System.out.println("1");
+        }
     }
 }
