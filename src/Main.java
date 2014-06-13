7e472ee6960c936b71d077fbdca3778a696819b0
xzhang <xzhang@bostontechnologies.com>
2014-06-12 23:01:34 -0400

    2014/06/12

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by xzhang71 on 6/3/14.
  */
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i <= 9999; i += 8) {
-            for (int j = 1; j <= 9999; j += 8) {
-                int t = (i + j) % 6;
-                if (t == 1) {
-                    System.out.println(i + ", " + j + ", " + t);
-                }
-            }
-        }
+        List<Integer> l = new ArrayList<>();
+        System.out.println(l.lastIndexOf(1));
     }
 }
---
+++ src/lee/RotateImage.java
@@ -0,0 +1,29 @@
+package lee;
+
+/**
+ * Created by xzhang71 on 6/12/14.
+ */
+public class RotateImage {
+
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
+        int length = matrix.length;
+
+        for (int i = 0; i <= (length - 1) / 2; i++) {
+            for (int j = i; j <= (length - 1) - i - 1; j++) {
+                rotatePoints(matrix, i, j);
+            }
+        }
+    }
+
+    private void rotatePoints(int[][] matrix, int i, int j) {
+        int l = matrix.length;
+        int[][] points = {{i, j}, {j, l - i - 1}, {l - i - 1, l - j - 1}, {l - j - 1, i}};
+        int temp = matrix[points[3][0]][points[3][1]];
+        int k = 3;
+        while (k >= 0) {
+            matrix[points[k][0]][points[k][1]] = k == 0 ? temp : matrix[points[k - 1][0]][points[k - 1][1]];
+            k--;
+        }
+    }
+}
